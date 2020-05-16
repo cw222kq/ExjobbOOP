3b11cec22bd29559e3f8e18e23e0893851f0970a
Charlotte Westerberg <cw222kq@student.lnu.se>
2020-05-16 16:46:35 +0200

    Add variables to hold the rgb values

--- Assets/Script/Cube.cs
+++ Assets/Script/Cube.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class Cube : MonoBehaviour
 {
     public GameObject prefab;
+    private float r;
+    private float g;
+    private float b;
     
     public void ChangeColors(List<GameObject> cubes) 
     {
         foreach (GameObject cube in cubes) {
+
+            r = Random.Range(0f, 1f);
+            g = Random.Range(0f, 1f);
+            b = Random.Range(0f, 1f);
         
-            cube.GetComponent<Renderer>().material.color = new Color(Random.Range(0f, 1f),Random.Range(0f, 1f),Random.Range(0f, 1f));           
+            cube.GetComponent<Renderer>().material.color = new Color(r, g, b);           
                       
         }
     }
