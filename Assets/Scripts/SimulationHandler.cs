cbf3318e80c32d604dd3341a699ab359b0525c30
Charlotte Westerberg <cw222kq@student.lnu.se>
2020-06-16 22:29:31 +0200

    Move the SpawnCubes method to the Update method

--- Assets/Scripts/SimulationHandler.cs
+++ Assets/Scripts/SimulationHandler.cs
@@ -7,25 +7,11 @@ public class SimulationHandler : MonoBehaviour
     // Variables
     private int frames;
     private float countdown;
+    private bool spawnedCubes;
     private static Cube cube;
     private static Spawner spawner;
     private static Explosion explosion;
     
-    // Checks the user input from the GUI. If width, deep and height is set to values those values will be used to set the numberOfCubes
-    // If width, deep and height are set to zero the building of cubes will consist of the value set in the numberOfCubes field
-    // Every complete level will consist of 100 cubes
-    void ProcessUserInput() 
-    {
-        if (spawner.width > 0 && spawner.deep > 0 && spawner.height > 0) 
-        {
-            spawner.SetMaxWidth(spawner.width);
-            spawner.SetMaxDeep(spawner.deep);
-            spawner.numberOfCubes = (spawner.width*spawner.deep)*spawner.height;
-        
-        }
-            
-    }  
-
     void Awake() // https://www.youtube.com/watch?v=fGshe3ILKnA
     {
         cube = GetComponent<Cube>();
@@ -38,8 +24,7 @@ public class SimulationHandler : MonoBehaviour
     void Start()
     {
         ProcessUserInput();
-        // UNCOMMENT THIS LINE BELOW!!!!!!!
-        spawner.SpawnCubes(cube); // Look into the DOD/DOTS version as this function is in the update method here. Makes OOP and DOD/DOTS call this method in the same function
+        
         // x sec after the building has been built up an explosion will occur
         countdown = explosion.delay;
         
@@ -48,6 +33,12 @@ public class SimulationHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!spawnedCubes)
+        {
+            // UNCOMMENT THIS LINE BELOW!!!!!!!
+            spawner.SpawnCubes(cube);
+            spawnedCubes = true;
+        }
         countdown -= Time.deltaTime;
         if (countdown <= 0f && !explosion.hasExplode) 
         {
@@ -66,4 +57,19 @@ public class SimulationHandler : MonoBehaviour
         }
         
     }
+
+    // Checks the user input from the GUI. If width, deep and height is set to values those values will be used to set the numberOfCubes
+    // If width, deep and height are set to zero the building of cubes will consist of the value set in the numberOfCubes field
+    // Every complete level will consist of 100 cubes
+    void ProcessUserInput() 
+    {
+        if (spawner.width > 0 && spawner.deep > 0 && spawner.height > 0) 
+        {
+            spawner.SetMaxWidth(spawner.width);
+            spawner.SetMaxDeep(spawner.deep);
+            spawner.numberOfCubes = (spawner.width*spawner.deep)*spawner.height;
+        
+        }
+            
+    }  
 }
