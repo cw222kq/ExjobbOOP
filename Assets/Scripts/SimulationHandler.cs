380c4918905274a9ddc796ed2fb880d478cf9159
Charlotte Westerberg <cw222kq@student.lnu.se>
2020-06-25 11:08:26 +0200

    Set the simulation to end after 25 seconds

--- Assets/Scripts/SimulationHandler.cs
+++ Assets/Scripts/SimulationHandler.cs
@@ -27,8 +27,8 @@ public class SimulationHandler : MonoBehaviour
         // x sec after the building has been built up an explosion will occur
         countdown = explosion.Delay;
 
-        // End simulation after 30 seconds
-        endSimulationTimer = 30f;
+        // End simulation after 25 seconds
+        endSimulationTimer = 25f;
         
     }
 
